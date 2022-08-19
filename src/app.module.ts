9f184745251407fc9138fb4af94965de9693180b
murilofarias <murilo145farias@gmail.com>
2022-08-19 18:21:04 -0300

    chore add helper routes to insert user and device

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FaixasController } from './faixas/faixas.controller';
-import { FaixasService } from './faixas/faixas.service';
 import { FaixasModule } from './faixas/faixas.module';
+import { EquipamentoSchema } from './infra/db/typeorm/equipamento.schema';
+import { UsuarioEquipamentoSchema } from './infra/db/typeorm/usuario-equipamento.schema';
+import { UsuarioSchema } from './infra/db/typeorm/usuario.schema';
 
 @Module({
   imports: [
@@ -18,7 +19,10 @@ import { FaixasModule } from './faixas/faixas.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
-    FaixasModule
+    FaixasModule,
+    TypeOrmModule.forFeature([UsuarioEquipamentoSchema]),
+    TypeOrmModule.forFeature([UsuarioSchema]), 
+    TypeOrmModule.forFeature([EquipamentoSchema]), 
   ],
   controllers: [AppController],
   providers: [AppService]
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Equipamento } from './@core/domain/equipamento';
+import { Usuario } from './@core/domain/usuario';
+import { EquipamentoSchema } from './infra/db/typeorm/equipamento.schema';
+import { UsuarioEquipamentoSchema } from './infra/db/typeorm/usuario-equipamento.schema';
+import { UsuarioSchema } from './infra/db/typeorm/usuario.schema';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(UsuarioEquipamentoSchema)
+    private usuarioEquipamentoRepository: Repository<UsuarioEquipamentoSchema>,
+    @InjectRepository(UsuarioSchema)
+    private usuarioRepository: Repository<UsuarioSchema>,
+    @InjectRepository(EquipamentoSchema)
+    private equipamentoRepository: Repository<EquipamentoSchema>){}
+  
+
+
+  async criarEquipamento(equipamento: Equipamento) {
+    const equipamentoSchema = this.equipamentoRepository.create(equipamento);
+    return this.equipamentoRepository.save(equipamentoSchema);
   }
+  
+  
+  async associarUsuarioEquipamento(usuario_id: string, equipamento_id: string) {
+    
+
+    const [usuario, equipamento] = await Promise.all([ 
+      this.usuarioRepository.findOneBy({
+        id: usuario_id
+      }),
+      this.equipamentoRepository.findOneBy({
+        id: equipamento_id
+      })
+    ])
+
+    let usuarioEquipamento;
+    if(usuario && equipamento){
+      usuarioEquipamento = this.usuarioEquipamentoRepository.save({
+        usuario: usuario,
+        equipamento: equipamento,
+        ativa: true
+      })
+    }
+    else
+      throw new HttpException("Usuário e Equipamento devem existir", HttpStatus.FORBIDDEN);
+
+    return usuarioEquipamento
+  }
+  
+  async criarUsuario(usuario: Usuario) {
+    const usuarioSchema = this.usuarioRepository.create(usuario);
+    return this.usuarioRepository.save(usuarioSchema);
+  }
+  
 }
