990481cfa9057f22904bd6b17b2ef1cda0da26bd
murilofarias <murilo145farias@gmail.com>
2022-08-20 22:01:46 -0300

    [HEL-50] feat: add repository to find Talao with basic options

--- src/@core/plug/faixa.repository.interface.ts
+++ src/@core/plug/faixa.repository.interface.ts
@@ -7,5 +7,32 @@ export interface FaixaRepositoryInterface{
     save(faixa:Faixa, taloes: Talao[]);
     find(condicoes): Promise<Faixa[]>;
     saveTaloes(faixa: Faixa, taloes: Talao[]);
+    findTaloes(options: MonitoramentoTaloesOptions): Promise<TalaoPage>;
     
+}
+
+export interface MonitoramentoTaloesOptions{
+    take: number;
+    skip: number;
+    tipo: number
+    userName: string
+    tenantId: string
+}
+
+export interface TalaoPage{
+    meta: Meta;
+    data: DataTalao[];
+}
+
+interface Meta{
+    total: number
+}
+
+export interface DataTalao{
+    id?: number
+    username?: string
+    ticketNumber: string
+    equipment: string
+    date: Date
+    status: number
 }
--- src/infra/db/typeorm/faixa.repository.ts
+++ src/infra/db/typeorm/faixa.repository.ts
@@ -3,8 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Faixa } from "src/@core/domain/faixa";
 import { Talao } from "src/@core/domain/talao";
 
-import { FaixaRepositoryInterface } from "src/@core/plug/faixa.repository.interface";
-import { Repository } from 'typeorm';
+import { FaixaRepositoryInterface, MonitoramentoTaloesOptions, TalaoPage, DataTalao } from "src/@core/plug/faixa.repository.interface";
+import { DataSource, Repository, SelectQueryBuilder } from 'typeorm';
 import { FaixaSchema } from "./faixa.schema";
 import { TalaoSchema } from "./talao.schema";
 
@@ -14,8 +14,53 @@ export class FaixaRepository implements FaixaRepositoryInterface{
         @InjectRepository(FaixaSchema)
         private faixaRepository: Repository<FaixaSchema>,
         @InjectRepository(TalaoSchema)
-        private talaoRepository: Repository<TalaoSchema>
+        private talaoRepository: Repository<TalaoSchema>,
+        private dataSource: DataSource
       ) {}
+
+
+    async findTaloes(options: MonitoramentoTaloesOptions): Promise<TalaoPage> {
+        let query: SelectQueryBuilder<TalaoSchema>=  this.dataSource
+            .createQueryBuilder()
+            .select("talao")
+            .from(TalaoSchema, "talao")
+            .leftJoinAndSelect('talao.usuario', 'usuario')
+            .leftJoinAndSelect('talao.equipamento', 'equipamento')
+            .leftJoinAndSelect('talao.faixa', 'faixa')
+            .where('faixa.tipoRegistro = :tipo', { tipo: options.tipo })
+            .andWhere('faixa.idTenant = :tenant', { tenant: options.tenantId })
+
+        if(options.userName)
+            query = query
+                .andWhere('usuario.nome like :nome', { nome: `%${options.userName}%`})    
+        else
+            query = query
+                .andWhere('faixa.tipoRegistro = :tipo', { tipo: options.tipo })
+
+        
+        const [result, total] =  await query
+            .orderBy("talao.identificador", "ASC")
+            .skip(options.skip)
+            .take(options.take)
+            .getManyAndCount()
+
+        const talaoData: DataTalao[] = result.map(talaoSchema => {
+            return {
+                username: talaoSchema.usuario ? talaoSchema.usuario.nome: null,
+                ticketNumber: talaoSchema.identificador,
+                equipment: talaoSchema.equipamento.imei,
+                date: talaoSchema.dataLiberacao,
+                status: talaoSchema.situacao
+            }
+        });
+
+        return {
+            meta: { total: total},
+            data: talaoData
+        }
+    }
+
+
     async find(condicoes): Promise<Faixa[]> {
         const faixasSchema = await this.faixaRepository.find(condicoes);
         return faixasSchema.map(faixasSchema => new Faixa(
@@ -48,5 +93,7 @@ export class FaixaRepository implements FaixaRepositoryInterface{
         this.faixaRepository.save(faixaSchema);
         return this.talaoRepository.save(taloesSchema) ;
     }
+
+    
 }
 
